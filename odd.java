75564cacf849994c03bd05528fe4c09f297ad70b
monisha13 <47376867+monisha13@users.noreply.github.com>
2019-02-06 00:04:29 -0800

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n=3;
+	if(n%2==0)
+	System.out.println("Even");
+	else if(n%2>0)
+	System.out.println("Odd");
+	else
+	System.out.println("invalid");
+	}
+}
